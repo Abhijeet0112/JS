521864c7142f6bfaedcaaf2a4b4431b50eb9bb53
ABHIJEET BHINGARDEVE <137824361+Abhijeet0112@users.noreply.github.com>
2025-06-01 18:26:56 +0000

    String Basics

---
+++ basicsOfJs/6_string.js
@@ -0,0 +1,43 @@
+const name = "Abhijeet Bhingardeve";
+const myRepoCount = 10;
+
+console.log(`Hello ! My name is ${name} and my repocount is ${myRepoCount}`);
+
+const homeName = new String ("Home Sweet Home - Ingrul");
+
+console.log(homeName[1]);
+console.log(homeName.length);
+
+console.log(homeName.toUpperCase());
+console.log(homeName);
+
+console.log(homeName.charAt(7));
+console.log(homeName.indexOf("t"));
+
+console.log(homeName.substring(5, 10));
+console.log(homeName.slice(5 , 10));
+
+const newString = "     Vikas Shinde"
+console.log(newString);
+console.log(newString.trim());//Remove starting and spaces
+
+const url = "http://abhijeet.gmail.com/group%20admin";
+console.log(url);
+
+console.log(url.replace("%20","-"));
+
+console.log(url.includes("abhijeet"));
+
+console.log(homeName.split("-"));
+
+console.log(homeName.repeat(5));
+
+
+
+
+
+
+
+
+
+
