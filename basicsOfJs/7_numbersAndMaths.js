ef6053e6c6bda9c6a183c8f42ce62cae5d48597c
ABHIJEET BHINGARDEVE <137824361+Abhijeet0112@users.noreply.github.com>
2025-06-02 06:30:32 +0000

    numbers and maths first commmit

---
+++ basicsOfJs/7_numbersAndMaths.js
@@ -0,0 +1,16 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number (100);
+console.log(balance);
+
+console.log(balance.toString());
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const number = 123.7366;
+
+console.log(number.toPrecision(3));
+
